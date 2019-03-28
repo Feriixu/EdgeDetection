6e0730b4c216630ae806832a74f02ca4530a95dc
Feriixu <feriixu@users.noreply.github.com>
2019-03-28 13:17:35 +0100

    Initial commit

---
+++ EdgeDetection/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace EdgeDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonStart.Location = new System.Drawing.Point(0, 427);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(800, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Do the thing";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ EdgeDetection/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ImageProcessor;
+using ImageProcessor.Imaging.Filters.EdgeDetection;
+
+namespace EdgeDetection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            //pictureBox1.BackgroundImage = CleanBitmap(new Bitmap("skyline.png"), 2, 0.5f, 8);
+            pictureBox1.BackgroundImage = new Bitmap("skyline.png");
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            ImageFactory imageFactory = new ImageFactory();
+            imageFactory.Load(pictureBox1.BackgroundImage);
+            IEdgeFilter edgeFilter = new SobelEdgeFilter();
+            imageFactory.DetectEdges(edgeFilter, true);
+            pictureBox1.BackgroundImage = imageFactory.Image;
+        }
+
+        private static Bitmap CleanBitmap(Bitmap raw, int thickness, float noiseThresh, int neighThresh)
+        {
+            Bitmap dirty = new Bitmap(raw);
+            for (int x = 0; x < dirty.Width; x++)
+            {
+                for (int y = 0; y < dirty.Height; y++)
+                {
+                    Color pixCol = dirty.GetPixel(x, y);
+                    if (pixCol.GetBrightness() > noiseThresh)
+                    {
+                        dirty.SetPixel(x, y, Color.White);
+                    }
+                    else
+                    {
+                        for (int i = -thickness; i < 1; i++)
+                        {
+                            for (int j = -thickness; j < 1; j++)
+                            {
+                                if ((x + i > dirty.Width) || (x + i < 0) || (y + j > dirty.Height) || (y + j < 0))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    dirty.SetPixel(x + i, y + j, Color.Black);
+                                }
+                            }
+                        }
+                    }
+
+                    if (x == 0 || x == dirty.Width - 1 || y == 0 || y == dirty.Height - 1)
+                    {
+                        dirty.SetPixel(x, y, Color.Black);
+                    }
+                }
+            }
+
+            for (int x = 1; x < dirty.Width - 1; x++)
+            {
+                for (int y = 1; y < dirty.Height - 1; y++)
+                {
+                    int neigh = 0;
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            if (dirty.GetPixel(x + i, y + j).GetBrightness() < noiseThresh)
+                            {
+                                neigh++;
+                            }
+                        }
+                    }
+
+                    if (neigh > neighThresh)
+                    {
+                        dirty.SetPixel(x, y, Color.Black);
+                    }
+                }
+            }
+
+            return new Bitmap(dirty);
+        }
+    }
+
+
+}
